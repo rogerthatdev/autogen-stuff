ae04f7cf4d91d50f6ae699cf313b93ed00b84c30
Roger Martinez <roger.that.dev@gmail.com>
2024-10-09 21:36:28 -0700

    use dotenv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import dotenv
 import logging
 from autogen_agentchat import EVENT_LOGGER_NAME
 from autogen_agentchat.agents import CodeExecutorAgent, CodingAssistantAgent
@@ -6,6 +7,8 @@ from autogen_agentchat.logging import ConsoleLogHandler
 from autogen_agentchat.teams import RoundRobinGroupChat, StopMessageTermination
 from autogen_core.components.code_executor import DockerCommandLineCodeExecutor
 from autogen_core.components.models import OpenAIChatCompletionClient
+
+dotenv.load_dotenv()
 
 logger = logging.getLogger(EVENT_LOGGER_NAME)
 logger.addHandler(ConsoleLogHandler())
